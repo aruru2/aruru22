296117caebed411d3bbb9b1fc80c651ea7116a25
aruru2 <104402628+aruru2@users.noreply.github.com>
2022-11-08 09:15:57 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,4 +1,4 @@
 function imgchange1()
 {
-document.getElementById('img1').innerHTML = '<img src="./image/城西大学２.jpg">';
+document.getElementById('img1').innerHTML = '<img src="./image/城西大学.jpg">';
 }
