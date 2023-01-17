14495c434654a0ecd2a837dd7f7c8fa2af23a52f
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-01-17 09:39:06 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,13 +1,5 @@
-function keisan(){ 
-  var x = document.getElementById("shincho").value; 
-  var y	= document.getElementById("taijuu").value; 
-  var z = y/( (x/100)*(x/100) );
- 
-
- if(z >= 25){
-   document.getElementById("kuuran").textContent = "肥満型"; }
-  else if(z < 25 && z >= 18.5){ 
-   document.getElementById("kuuran").textContent = "標準型"; }
-  else{ 
-   document.getElementById("kuuran").textContent = "瘦せ型";}
- }
+function add73(){
+var x =document.getElementById('input3').value;
+var y =document.getElementById('input4').value;
+alert(Number(y)/Number(x*x)*10000);
+}
