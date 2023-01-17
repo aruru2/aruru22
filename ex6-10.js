ebda9ace2d342c39020637bbc2dd66ee3edcd8dc
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-01-17 10:36:01 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -5,5 +5,5 @@ document.getElementById('logo').src='./image/城西大学.png';
 
 function imgchange2()
 {
-document.getElementById('logo').src='./image/城西大学.png';
+document.getElementById('logo').src='./image/城西大学1.png';
 }
